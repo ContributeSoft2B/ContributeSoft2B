e42f0f9e7e9051e37759d3d3921f8db37d6bfa95
校建强 <isjianqiang@outlook.com>
2018-01-05 20:24:19 +0800

    suc

--- ContributeComponents/Domains/Applications.cs
+++ ContributeComponents/Domains/Applications.cs
@@ -12,29 +12,29 @@ namespace ContributeComponents.Domains
     {
         public int Id { get; set; }
         [StringLength(255,ErrorMessage = "The content must not exceed 255 characters")]
-        [Required(AllowEmptyStrings = false,ErrorMessage = "必填项")]
+        [Required(AllowEmptyStrings = false,ErrorMessage = "required")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
-        [StringLength(255, ErrorMessage = "内容不得超过255个字符")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "必填项")]
+        [StringLength(255, ErrorMessage = "The content must not exceed 255 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "required")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        [StringLength(255, ErrorMessage = "内容不得超过255个字符")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "必填项")]
+        [StringLength(255, ErrorMessage = "The content must not exceed 255 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "required")]
         [Display(Name = "Full Job Title")]
         public string FullJobTitle { get; set; }
-        [StringLength(255, ErrorMessage = "内容不得超过255个字符")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "必填项")]
-        [RegularExpression(@"^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "请输入正确的Email")]
+        [StringLength(255, ErrorMessage = "The content must not exceed 255 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "required")]
+        [RegularExpression(@"^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is invalid or already taken")]
         [Display(Name = "Email")]
         public string Email { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "必填项")]
-        [RegularExpression(@"^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$", ErrorMessage = "请输入正确的手机号码")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "required")]
+        [RegularExpression(@"^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$", ErrorMessage = "Phone is invalid or already taken")]
         [Display(Name = "Phone")]
 
         public string Phone { get; set; }
         [Display(Name = "Telegram")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "必填项")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "required")]
         public string  Telegram { get; set; }
         public DateTime CreateTime { get; set; }
     }
