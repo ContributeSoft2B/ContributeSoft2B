98b18c50f51a8e711477a10fd6a5ec83e5cbcf0d
杜鹏 <dupeng.tommy@outlook.com>
2018-01-30 18:34:26 +0800

    *用户发送ETH地址，创建用户

---
+++ Contribute/Controllers/TelegramController.cs
@@ -0,0 +1,30 @@
+﻿using ContributeComponents.Repositories.Ef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContributeComponents.Domains;
+
+namespace Contribute.Controllers
+{
+    public class TelegramController : Controller
+    {
+        private ContributeDbContext db = new ContributeDbContext();
+        // GET: Telegram
+        public ActionResult Index(string ethAddress,int parentId=0)
+        {
+            var telegram = new Telegrams
+            {
+                EthAddress= ethAddress,
+                CreateTime=DateTime.UtcNow,
+                InviteUrl="https://www.baidu.com",
+                ParentId= parentId,
+                VerificationCode= $"/code {Guid.NewGuid()}"
+            };
+            db.Telegrams.Add(telegram);
+            db.SaveChanges();
+            return Json(new {result = "注册成功"},JsonRequestBehavior.AllowGet);
+        }
+    }
+}
