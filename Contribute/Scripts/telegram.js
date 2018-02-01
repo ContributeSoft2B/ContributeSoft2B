145fde08dfe453039f49064b6a8981713bf637d3
校建强 <isjianqiang@outlook.com>
2018-02-01 11:06:39 +0800

    tips

--- Contribute/Scripts/telegram.js
+++ Contribute/Scripts/telegram.js
@@ -5,7 +5,7 @@
     });
     function copyToClipboard(e, txt, copy_ele) {
         var clipboardData = window.clipboardData || e.clipboardData;
-
+        $('.pop-tip').show();
         if (clipboardData) {
             clipboardData.clearData();
             clipboardData.setData("text/plain", txt);
@@ -18,8 +18,11 @@
             selection.addRange(range);
             document.execCommand('copy');
         } else {
-            alert('您的浏览器不支持此方法，请手动复制');
+            //$('p.tip-txt').text('复制失败，请手动');
         }
+        setTimeout(function () {
+            $('.pop-tip').hide();
+        },3000);
     }
 
     $('input.btn-sub').on('click', function () {
@@ -33,7 +36,11 @@
                 alert('Please enter your ETH wallet address');
             }
             return false;
+        } else if (eAddr.substr(0,2) != "0x"){
+            alert('请输入正确的地址');
+            return false;
         }
+        
         $(this).prop('disabled');
         $.ajax({
             url: '../Telegram/Index',
@@ -41,18 +48,30 @@
             data: { parentId: parId, ethAddress: eAddr },
             success: function (data) {
                 console.log('成功');
+                $('.pop-tip').show();
+                if (data.success == false) {
+                } else {
+                    $('.pop-tip p').text("注册成功");
+                }
                 setTimeout(function () {
                     if (onEn) {
                         location.href = "../Telegram/DetailEn?code=" + data.VerificationCode;
                     } else {
                         location.href = "../Telegram/Detail?code=" + data.VerificationCode;
                     }
-                });
+                },4000);
             },
-            err: function () {
+            error: function () {
                 console.log('失败');
+                $('.pop-tip').show().find('p').text("注册失败，请重试");
+                setTimeout(function () {
+                    $('.pop-tip').hide();
+                },2000);
             }
         });
         //return false;
     });
+    $('.modal').on('click', function () {
+        $(this).hide();
+    })
 });
