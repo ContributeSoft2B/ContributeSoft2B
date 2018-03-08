372e9fedb7bdc9f9989b5e13ed097dbd9574aba3
杜鹏 <dupeng.tommy@outlook.com>
2018-03-08 17:44:09 +0800

    增加管理员权限

--- Contribute/Controllers/HomeController.cs
+++ Contribute/Controllers/HomeController.cs
@@ -107,7 +107,11 @@ DATA:{4}
             {
                 url = $"https://www.soft2b.com/telegram/Verification?verificationCode=";
             }
-
+            if (message.From.Username== "liuhao_blockchain"|| message.From.FirstName.Contains("STB") )
+            {
+                logger.Debug($"管理员消息{message.MessageId}处理完毕！~");
+                return new HttpStatusCodeResult(200, "已处理");
+            }
             if (message.Type == MessageType.TextMessage && message.Text.Contains("http") &&
                 !message.Text.Contains("soft2b"))
             {
@@ -147,31 +151,39 @@ DATA:{4}
             }
             if (message.Type == MessageType.PhotoMessage)
             {
-                await Bot.Api.DeleteMessageAsync(message.Chat.Id, message.MessageId);
-            }
-            if (message.Type == MessageType.TextMessage && message.Text.StartsWith("/code"))
-            {
-                url = $"{url}{message.Text}";
-                // Echo each Message
-                using (WebClient client = new WebClient())
+                //await Bot.Api.DeleteMessageAsync(message.Chat.Id, message.MessageId);
+                var deleteMessageResult = await Bot.Api.KickChatMemberAsync(message.Chat.Id, message.From.Id);
+                if (!deleteMessageResult)
                 {
-                    client.Encoding = Encoding.UTF8;
-                    var resultJson = client.DownloadString(url);
-                    var result = new { Success = false, Msg = string.Empty, InviteUrl = string.Empty };
-                    result = JsonConvert.DeserializeAnonymousType(resultJson, result);
-                    if (result.Success == true)
-                    {
-                        await Bot.Api.SendTextMessageAsync(message.Chat.Id, result.Msg, ParseMode.Default, false,
-                            false, message.MessageId);
-                    }
-                    else
-                    {
-                        await Bot.Api.SendTextMessageAsync(message.Chat.Id, result.Msg, ParseMode.Default, false,
-                            false, message.MessageId);
-                    }
-
+                    logger.Debug($"消息{message.MessageId}不存在，踢出失败！~");
+                    return new HttpStatusCodeResult(200, "踢出失败");
                 }
+                logger.Debug($"消息{message.MessageId}踢出成功！~");
+                return new HttpStatusCodeResult(200, "踢出成功");
             }
+            //if (message.Type == MessageType.TextMessage && message.Text.StartsWith("/code"))
+            //{
+            //    url = $"{url}{message.Text}";
+            //    // Echo each Message
+            //    using (WebClient client = new WebClient())
+            //    {
+            //        client.Encoding = Encoding.UTF8;
+            //        var resultJson = client.DownloadString(url);
+            //        var result = new { Success = false, Msg = string.Empty, InviteUrl = string.Empty };
+            //        result = JsonConvert.DeserializeAnonymousType(resultJson, result);
+            //        if (result.Success == true)
+            //        {
+            //            await Bot.Api.SendTextMessageAsync(message.Chat.Id, result.Msg, ParseMode.Default, false,
+            //                false, message.MessageId);
+            //        }
+            //        else
+            //        {
+            //            await Bot.Api.SendTextMessageAsync(message.Chat.Id, result.Msg, ParseMode.Default, false,
+            //                false, message.MessageId);
+            //        }
+
+            //    }
+            //}
             logger.Debug($"消息{message.MessageId}处理完毕！~");
             return new HttpStatusCodeResult(200, "已处理");
         }
